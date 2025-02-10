9f096eb9298b5d3569f84dc7182fe5a821e50552
Fontii99 <kfmpau@gmail.com>
2025-02-10 23:24:19 +0100

    Agregar archivos de proyecto.

---
+++ TrackIt/UI/ExpenseMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TrackIt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ExpenseMenu : Window
+    {
+        public ExpenseMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TrackIt/UI/MainMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TrackIt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
