d3464da27bacd482c7951cd2e37f923688e8753a
pfont2 <pfont2@ESONEPC0JFN2T>
2025-02-26 16:44:25 +0100

    Added End Data field

--- TrackIt/UI/View/ExpenseMenu.xaml.cs
+++ TrackIt/UI/View/ExpenseMenu.xaml.cs
@@ -18,12 +18,15 @@ namespace TrackIt
         private void RecurringCheckBox_Checked(object sender, RoutedEventArgs e)
         {
             PeriodComboBox.IsEnabled = true;
+            EndDatePicker.IsEnabled = true;
             
         }
 
         private void RecurringCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
             PeriodComboBox.IsEnabled = false;
+            EndDatePicker.IsEnabled = false;
+            EndDatePicker.SelectedDate = null;
             PeriodComboBox.SelectedIndex = -1;
         }
 
